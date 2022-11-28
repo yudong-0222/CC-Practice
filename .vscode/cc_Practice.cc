a7c4bb400b2056eab0e33115a9a0acc8a8235b4c
RedFeaTherXll <yuchi9716@gmail.com>
2022-11-28 19:25:56 +0800

    add gitignore

--- .vscode/cc_Practice.cc
+++ .vscode/cc_Practice.cc
@@ -4,11 +4,5 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  string s,t;
-  getline(cin,s);
-  for (int i =0; i <s.size(); i++ ) {
-    s[i] += 4;
-  }
-  cout << s << "\n";
+ saf
 }
